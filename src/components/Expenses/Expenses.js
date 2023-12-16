eec28da1d392aa0de84598b394bdeaac642dddb1
Andrew Sapegin <andrei.sapegin@farfetch.com>
2023-12-16 23:00:06 +0000

    Expenses

---
+++ src/components/Expenses/Expenses.js
@@ -0,0 +1,35 @@
+import React, { useState } from "react";
+import "./Expenses.css";
+import Card from "../UI/Card";
+import ExpensesFilter from "./ExpensesFilter";
+import ExpensesList from "./ExpensesList";
+import ExpensesChart from "./ExpensesChart";
+
+const Expenses = (props) => {
+  const [filteredYear, setFilteredYear] = useState("2021");
+
+  const filterChangeHandler = (selectedYear) => {
+    setFilteredYear(selectedYear);
+  };
+
+  const filteredExpenses = props.expenses.filter((exp) => {
+    return exp.date.getFullYear().toString() === filteredYear;
+  });
+
+  return (
+    <div>
+      <li>
+        <Card className="expenses">
+          <ExpensesFilter
+            selected={filteredYear}
+            onChangeFilter={filterChangeHandler}
+          />
+          <ExpensesChart expenses={filteredExpenses}/>
+          <ExpensesList expenses={filteredExpenses} />
+        </Card>
+      </li>
+    </div>
+  );
+};
+
+export default Expenses;
